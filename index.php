392d4c2aaa9be3d0e41dee84d7f47fb90e3dcb2e
Shoukat Mirza <shoukat.bjr@gmail.com>
2017-07-10 11:49:32 +0500

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                      // Finds extensions of files
                      function findexts ($filename) {
                         $filename=strtolower($filename);
-                        $exts=split("[/\\.]", $filename);
+                        $exts=explode("[/\\.]", $filename);
                         $n=count($exts)-1;
                         $exts=$exts[$n];
                         return $exts;
